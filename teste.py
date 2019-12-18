80551b3fa722f19fde192da32cfacce498a2f3d9
Franklin Ribeiro <franklin.ribeiro@scielo.org>
2019-12-18 16:02:59 -0300

    Adicionando comandos de instalar pacotes

--- teste.py
+++ teste.py
@@ -1,3 +1,24 @@
-import subprocess
+#!/usr/bin/python
+# coding: utf-8
 
-subprocess.Popen('sudo apt-get -y install nginx', shell=True, stdin=subprocess.PIPE)
+import subprocess
+import pdb
+
+if __name__ == "__main__":
+
+
+    ## Atualizando pacotes
+    subprocess.Popen('sudo apt-get update', shell=True, stdin=subprocess.PIPE)
+    
+    
+    apt = "sudo apt-get -y install "
+    packages = "nginx apt-transport-https ca-certificates curl gnupg2 software-properties-common"
+
+    print("Instalando pacotes necessarios para o NGINX e o Docker")
+
+    for package in packages.split():
+        command = apt + package
+        p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
+        p_status = p.wait()
+
+    
